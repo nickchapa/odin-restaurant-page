58631fb8f5ab63316c82f2c633c4fedd2971e0c3
Nick Chapa <nicky.chapa@hotmail.com>
2026-07-21 10:30:48 -0500

    add tab modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles.css";
+import {home} from "./home.js";
+import {menu} from "./menu.js";
+import {contact} from "./contact.js";
 
-console.log('test');
+console.log(home);
+console.log(menu);
+console.log(contact);
