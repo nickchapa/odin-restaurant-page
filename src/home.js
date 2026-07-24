14fcdefb1f022a6bcfadde5cc57966e5c9313a42
Nick Chapa <nicky.chapa@hotmail.com>
2026-07-24 08:10:27 -0500

    add homepage content

--- src/home.js
+++ src/home.js
@@ -5,22 +5,76 @@ export function homeTab(){
 
     const image = document.createElement('img');
     const restName = document.createElement('h1');
-    const description = document.createElement('p');
 
     image.src = restaurantImage;
     restName.textContent = 'My Restaurant';
-    description.textContent = 'restaurant description';
 
-    description.style.color = 'red';
-
-    image.style.width = '100px';
-    image.style.height = '100px';
+    setImageSize(image);
 
     divContent.append(image);
     divContent.append(restName);
-    divContent.append(description);
+    divContent.append(descriptionDiv());
+    divContent.append(hoursDiv());
+    divContent.append(locationDiv());
 }
 
 function setImageSize(img){
-    img.style.width = '100';
+    img.style.width = '100px';
+    img.style.height = '100px';
+}
+
+function descriptionDiv(){
+    const div = document.createElement('div');
+    const description = document.createElement('p');
+    const signature = document.createElement('h2');
+
+    description.textContent = 'restaurant description';
+    signature.textContent = 'signature';
+
+    div.append(description);
+    div.append(signature);
+
+    return div;
+}
+
+function hoursDiv(){
+    const hoursDiv = document.createElement('div');
+    const title = document.createElement('h2');
+    const hours = document.createElement('div');
+    const weekends = document.createElement('p');
+    const weekdays = document.createElement('p');
+
+    // set textcontent
+    title.textContent = 'Hours';
+    weekends.textContent = 'Weekends: Closed';
+    weekdays.textContent = 'Weekdays: 9am - 5pm';
+
+    // append to hoursDiv
+    hours.append(weekends);
+    hours.append(weekdays);
+    hoursDiv.append(title);
+    hoursDiv.append(hours);
+
+    return hoursDiv;
+}
+
+function locationDiv(){
+    //create div
+    //create h2
+    //create p
+
+    const locationDiv = document.createElement('div');
+    const title = document.createElement('h2');
+    const address = document.createElement('p');
+
+    //h2, p text content
+    title.textContent = 'Location';
+    address.textContent = '123 Choco Lane, Princessland';
+
+    //append h2, p to div
+    locationDiv.append(title);
+    locationDiv.append(address);
+
+    //return div
+    return locationDiv;
 }
