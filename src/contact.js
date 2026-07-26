1dc20e370a1829a2a6a4a597efce0f59a81e1b63
Nick Chapa <nicky.chapa@hotmail.com>
2026-07-26 13:05:07 -0500

    create basic contact content

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,51 @@
-export const contact = 'contact';
+export function contactTab(){
+    const divContent = document.querySelector('#content');
+
+    const contactTitle = document.createElement('h1');
+    contactTitle.textContent = 'Contact';
+    
+    divContent.append(contactTitle);
+
+    divContent.append(contactItem(
+        'name',
+        'position',
+        'phone',
+        'email'
+    ));
+
+    divContent.append(contactItem(
+        'name',
+        'position',
+        'phone',
+        'email'
+    ));
+
+    divContent.append(contactItem(
+        'name',
+        'position',
+        'phone',
+        'email'
+    ));
+}
+
+function contactItem(name, position, phone, email){
+    const div = document.createElement('div');
+    const nameE = document.createElement('h3');
+    const positionE = document.createElement('p');
+    const phoneE = document.createElement('p');
+    const emailE = document.createElement('p');
+
+    nameE.textContent = name;
+    positionE.textContent = position;
+    phoneE.textContent = phone;
+    emailE.textContent = email;
+
+    div.className = 'contactItem';
+
+    div.append(nameE);
+    div.append(positionE);
+    div.append(phoneE);
+    div.append(emailE);
+
+    return div;
+}
